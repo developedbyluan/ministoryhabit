cae74d9ce51e91cba2ff0a2ac0ddeece62c4a87a
Luan Tran <developedbyluan@gmail.com>
2025-01-24 10:21:16 +0700

    Allow images loaded from cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,29 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-async() => {
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
-}
+async () => {
+  if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
+  }
+};
 
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
