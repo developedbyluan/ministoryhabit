ad9a0f57a4b04a5faefe6d1df36025f7520d2219
Luan Tran <developedbyluan@gmail.com>
2025-01-24 12:05:03 +0700

    kinde: redirect to a destination dynamically

--- src/app/(root)/v/[slug]/page.tsx
+++ src/app/(root)/v/[slug]/page.tsx
@@ -8,12 +8,13 @@ import {
 import { useState } from "react";
 
 export default function VideoPage() {
-    const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
   const { isAuthenticated, isLoading, user } = useKindeBrowserClient();
-  console.log(isAuthenticated)
+  console.log(isAuthenticated);
 
   if (isLoading) return <div>Loading...</div>;
 
+  // https://docs.kinde.com/developer-tools/sdks/backend/nextjs-sdk/#redirecting-after-authentication
   return isAuthenticated ? (
     <div>
       <h1>Video</h1>
@@ -22,15 +23,22 @@ export default function VideoPage() {
     </div>
   ) : (
     <div>
-        <Input 
-            type="text"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-        />
-      You have to <LoginLink authUrlParams={{
-        connection_id: process.env.KINDE_CONNECTION_ID_OTP!,
-        login_hint: email
-      }}>Login</LoginLink> to see this page
+      <Input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      You have to{" "}
+      <LoginLink
+        postLoginRedirectURL="/v/trump"
+        authUrlParams={{
+          connection_id: "conn_0194647b282b904ba592cd725888b0f6",
+          login_hint: email,
+        }}
+      >
+        Login
+      </LoginLink>{" "}
+      to see this page
     </div>
   );
 }
