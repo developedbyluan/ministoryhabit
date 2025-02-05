64df23e2f573752d988f09087990d1f661d6703c
Luan Tran <developedbyluan@gmail.com>
2025-02-05 12:37:07 +0700

    feat (actions/translate): add AI GATEWAY

--- src/app/actions/translate.ts
+++ src/app/actions/translate.ts
@@ -1,12 +1,19 @@
 "use server";
 
 import { generateText } from "ai";
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 
 export async function translateText(text: string) {
   try {
+    const openai = createOpenAI({
+      // custom settings, e.g.
+      compatibility: "strict", // strict mode, enable when using the OpenAI API
+      baseURL: process.env.AI_GATEWAY,
+    });
+
     const { text: translation } = await generateText({
-      model: openai("gpt-4"),
+      model: openai("gpt-4o-mini"),
+      temperature: 1,
       prompt: `Translate the following English text to Vietnamese. Only return the translation, nothing else: "${text}"`,
     });
 
